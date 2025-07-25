4772221264fac11976d4107788e6c9ff51c6410a
ratataque <ewanrimmely@outlook.fr>
2025-07-25 15:22:52 +0200

    update noice and mason ensure enstalled

--- lua/plugins/mason.lua
+++ lua/plugins/mason.lua
@@ -1,11 +1,38 @@
 return {
   {
     "mason-org/mason.nvim",
+    config = {
+      ensure_installed = {
+        "prettier",
+        "black",
+        "gofumpt",
+        "goimports-reviser",
+        "kulala-fmt",
+        "prettierd",
+        "pretty-php",
+        "stylua",
+      },
+    },
+  },
+  {
     "neovim/nvim-lspconfig",
   },
   {
     "mason-org/mason-lspconfig.nvim",
     event = "VeryLazy",
-    config = {},
+    config = {
+      ensure_installed = {
+        "rust_analyzer",
+        "lua_ls",
+        "gopls",
+        "dockerls",
+        "cssls",
+        "docker_compose_language_service",
+        "html",
+        "intelephense",
+        "marksman",
+        "ts_ls",
+      },
+    },
   },
 }
--- lua/plugins/noice.lua
+++ lua/plugins/noice.lua
@@ -4,12 +4,16 @@ return {
     event = "VeryLazy",
     opts = {
       lsp = {
-        hover = { enabled = false }, -- <-- HERE!
-        signature = { enabled = false }, -- <-- HERE!
+        -- hover = { enabled = false }, -- <-- HERE!
+        -- signature = { enabled = false }, -- <-- HERE!
+        ["vim.lsp.util.convert_input_to_markdown_lines"] = true,
+        ["vim.lsp.util.stylize_markdown"] = true,
       },
       presets = {
-        bottom_search = true, -- use a classic bottom cmdline for search
+        bottom_search = false, -- use a classic bottom cmdline for search
+        command_palette = true,
         long_message_to_split = true, -- long messages will be sent to a split
+        lsp_doc_border = true,
       },
     },
     dependencies = {
