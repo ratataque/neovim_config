4ebc229a57b4b3899b62cd0c378b223cc22435df
ratataque <ewanrimmely@outlook.fr>
2026-01-05 15:55:59 +0100

    update yazi

--- lua/plugins/yazi.lua
+++ lua/plugins/yazi.lua
@@ -1,36 +1,37 @@
 return {
-  ---@type LazySpec
-  {
-    "mikavilpas/yazi.nvim",
-    -- event = "VeryLazy",
-    cmd = { "Yazi" },
-    keys = {
-      -- 👇 in this section, choose your own keymappings!
-      {
-        "cd",
-        mode = { "n" },
-        "<cmd>Yazi<cr>",
-        desc = "Open yazi at the current file",
-      },
-      {
-        -- Open in the current working directory
-        "<leader>o",
-        "<cmd>Yazi cwd<cr>",
-        desc = "Open the file manager in nvim's working directory",
-      },
-      {
-        "<leader>y",
-        "<cmd>Yazi toggle<cr>",
-        desc = "Resume the last yazi session",
-      },
-    },
-    ---@type YaziConfig
-    opts = {
-      -- if you want to open yazi instead of netrw, see below for more info
-      open_for_directories = true,
-      keymaps = {
-        show_help = "<f1>",
-      },
-    },
-  },
+	---@type LazySpec
+	{
+		"mikavilpas/yazi.nvim",
+		-- event = "VeryLazy",
+		cmd = { "Yazi" },
+		keys = {
+			-- 👇 in this section, choose your own keymappings!
+			{
+				"cd",
+				mode = { "n" },
+				"<cmd>Yazi<cr>",
+				desc = "Open yazi at the current file",
+			},
+			{
+				-- Open in the current working directory
+				"<leader>o",
+				"<cmd>Yazi cwd<cr>",
+				desc = "Open the file manager in nvim's working directory",
+			},
+			{
+				"<leader>y",
+				"<cmd>Yazi toggle<cr>",
+				desc = "Resume the last yazi session",
+			},
+		},
+		---@type YaziConfig
+		opts = {
+			-- if you want to open yazi instead of netrw, see below for more info
+			open_for_directories = true,
+			keymaps = {
+				show_help = "<f1>",
+			},
+			open_multiple_tabs = true,
+		},
+	},
 }
