b2efea805cf9c9e5cc9ac22a1b2702d2f53b0b37
ratataque <ewanrimmely@outlook.fr>
2026-02-13 11:10:05 +0100

    added git dif from lazygit

--- lua/plugins/lazygit.lua
+++ lua/plugins/lazygit.lua
@@ -1,6 +1,19 @@
 return {
-  {
-    "kdheepak/lazygit.nvim",
-    cmd = "LazyGit",
-  },
+	"kdheepak/lazygit.nvim",
+	cmd = "LazyGit",
+	init = function()
+		function _G.DiffviewFromLazygit()
+			vim.schedule(function()
+				vim.cmd "bd!"
+				local f = io.open "/tmp/nvim_diffview_target"
+				if f then
+					local name = f:read("*a"):gsub("%s+$", "")
+					f:close()
+					os.remove "/tmp/nvim_diffview_target"
+					vim.cmd("DiffviewOpen -- " .. vim.fn.fnameescape(name))
+				end
+			end)
+			return ""
+		end
+	end,
 }
